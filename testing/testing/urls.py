52ef4f627e4c238f9785945bd5ac40362f3fb880
Shikhar Chauhan <shikhar.chauhan@live.com>
2017-05-24 19:48:58 +0530

    Adding docs

--- testing/testing/urls.py
+++ testing/testing/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',  include("epilogue.urls")),
-    url(r'^auth/' , include("rest_auth.urls"))
+    url(r'^auth/' , include("rest_auth.urls")),
+    url(r'^docs/', include('rest_framework_docs.urls')),
 ]
