6cf7f6e6156fab783d42e0539732032136a0c6a9
Shikhar Chauhan <shikhar.chauhan@live.com>
2017-07-06 22:35:18 +0530

    Adding args print to CustomerAuthentication

--- testing/epilogue/authentication.py
+++ testing/epilogue/authentication.py
@@ -5,5 +5,7 @@ class CustomerAuthentication(TokenAuthentication):
 	model = Token
 
 	def __init__(self , *args , **kwargs):
+		print(args)
+		print(kwargs)
 		print("Calling Customer Authentication ")
 		super().__init__(*args , **kwargs)
