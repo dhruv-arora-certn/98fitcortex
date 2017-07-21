c36055422937557e3a7e4e9e4dcf969fd470da75
Shikhar Chauhan <shikhar.chauhan@live.com>
2017-07-21 21:08:55 +0530

    Query for expanding Food list based on sizes and weights

--- testing/test.py
+++ testing/test.py
@@ -1,6 +1,6 @@
 from dietplan.goals import Goals
 from dietplan.utils import annotate_food
-from epilogue.models import Food
+from epilogue.models import *
 from dietplan.calculations import Calculations
 from dietplan.bodyTypes import BodyTypes
 from dietplan.activity import ActivityLevel
@@ -9,7 +9,7 @@ from dietplan.gender import Male,Female
 from dietplan.generator import Pipeline
 from dietplan.medical_conditions import Osteoporosis , Anemia
 from knapsack.knapsack_dp import knapsack,display
-
+from epilogue.replacement import *
 
 weight = 46 
 height = 1.6
@@ -19,3 +19,9 @@ gender = Female.number
 exclude = []
 p = Pipeline(weight , height , activity , goal , gender , disease = Anemia)
 c = Calculations(weight , height , activity , goal , gender , exclude)
+
+c = Customer.objects.get(pk = 8)
+g = c.dietplans.last()
+gf = GeneratedDietPlanFoodDetails.objects.filter(dietplan = g).filter(meal_type = 'm1')
+dish = gf.last()
+r = ReplacementPipeline(dish= dish , replaceMeal = True)
