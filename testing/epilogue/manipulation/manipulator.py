5cffc8599ce1207af83ff57f9810048cbccfbe59
Shikhar Chauhan <shikhar.chauhan@live.com>
2017-07-23 01:18:33 +0530

    Ground Work for cross join

--- testing/epilogue/manipulation/manipulator.py
+++ testing/epilogue/manipulation/manipulator.py
@@ -1,4 +1,15 @@
 from epilogue.models import Food , FoodTypeSizes
+from django.db.models import connection
+import lego
+import itertools
+
+class DummyFood:
+	'''
+	Class To Contain Dummy Food objects after cross join
+	'''
+	@lego.assemble
+	def __init__(self , *args , **kwargs):
+		pass
 
 class Manipulator():
 	
@@ -9,6 +20,18 @@ class Manipulator():
 
 	def categorize(self):
 		m = [map(e.categorize , self.items) for e in self.categorizers]
-		for e in m:
-			self.categorized_data.update(e)
+		a = itertools.chain(*m)
+		for e in a:
+			x,y = e.popitem()
+			if self.categorized_data.get(x):
+				self.categorized_data[x].append(y)
+			else:
+				self.categorized_data[x] = []
+				self.categorized_data[x].append(y)
+		return self
+
+	@property
+	def types(self):
+		l = set(self.categorized_data.keys())
+		return l - set(['uncategorized'])
 					
