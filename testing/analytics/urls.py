75b0085a4f35f68281dd9d5d27b3971c78b13fe6
Shikhar Chauhan <shikhar.chauhan@live.com>
2017-09-08 18:52:33 +0530

    Adding Event Tracking Views

--- testing/analytics/urls.py
+++ testing/analytics/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
-from analytics.views import CustomerGoogleClientView,CustomerTrackingView
+from analytics.views import CustomerGoogleClientView,CustomerTrackingView , EventPageTrackingView
 
 
 urlpatterns = [
 	url(r'^client/$' , CustomerGoogleClientView.as_view() , name = "customer-google-client"),
-	url(r'^track/$' , CustomerTrackingView.as_view() , name = "customer-tracking")
+	url(r'^track/$' , CustomerTrackingView.as_view() , name = "customer-tracking"),
+	url(r'^event-track/$' , EventPageTrackingView.as_view() , name = "event-tracking")
+
 ]
