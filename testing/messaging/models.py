1557d4a858aab59bd0bd204d92aee298bc456890
Shikhar Chauhan <shikhar.chauhan@live.com>
2017-09-19 15:10:07 +0530

    SMS Tracking

--- testing/messaging/models.py
+++ testing/messaging/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class SMSTracking(models.Model):
-	phone = models.CharField(max_length = 10)
+	phone = models.CharField(max_length = 10 , db_index = True)
 	message = models.CharField(max_length = 255)
 	saved = models.DateTimeField(auto_now_add = True)
