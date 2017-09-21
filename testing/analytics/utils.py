0f4abb4879a39e5572f903a7e385f199bbd8c080
Shikhar Chauhan <shikhar.chauhan@live.com>
2017-09-21 16:22:44 +0530

    Fix

--- testing/analytics/utils.py
+++ testing/analytics/utils.py
@@ -5,11 +5,13 @@ from django.template.loader import render_to_string
 
 
 def send_navratri_day_email(self):
-	user_list = UserSignupSource.objects.values("customer__email" , "customer__first_name" , flat =True)
+	user_list = UserSignupSource.objects.filter(customer__create_on__day = 20 , customer__create_on__month = 9 , customer__create_on__year = 2017).values("customer__email" , "customer__first_name")
+	l = []
 	for e in user_list:
 		em = EmailMessage(
 			subject = "Day 1 Healthy Navratri Diet Plan | 98Fit",
 			recipient = [e['customer__email']],
 			message = render_to_string("navratri-day-1.html")
 		)
-	return user_list
+		l.append([e['customer__email'] , em.send()])
+	return l
