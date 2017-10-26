6faf56baff4fd43802c19f3753370b7f9ead8a4c
Shikhar Chauhan <shikhar.chauhan@live.com>
2017-10-26 14:37:55 +0530

    Adding user level to /auth/user/

--- testing/epilogue/serializers.py
+++ testing/epilogue/serializers.py
@@ -19,7 +19,7 @@ class CustomerSerializer(serializers.ModelSerializer):
 
 	class Meta:
 		model = Customer
-		fields = ["email" , "first_name" , "last_name" , "mobile" , "age" , "weight" , "height", "lifestyle" , "objective" , "id", "gender" , "body_type" , "food_cat" ,"weight_type" , "height_type"]
+		fields = ["email" , "first_name" , "last_name" , "mobile" , "age" , "weight" , "height", "lifestyle" , "objective" , "id", "gender" , "body_type" , "food_cat" ,"weight_type" , "height_type" , "level"]
 
 
 class FoodSerializer(serializers.ModelSerializer):
