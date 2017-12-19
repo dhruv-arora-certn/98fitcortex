5034d9952f7d8dffc80f15aab2d547101cd8ae4b
Shikhar Chauhan <shikhar.chauhan@live.com>
2017-12-19 19:39:44 +0530

    Adding Regeneration Model and signal connecting

--- testing/regeneration/models.py
+++ testing/regeneration/models.py
@@ -2,3 +2,21 @@ from django.db import models
 
 # Create your models here.
 from .receivers import *
+
+
+
+class RegenerationLog(models.Model):
+	DIET = 'diet'
+	WORKOUT = 'workout'
+	TYPE_CHOICES = (
+		(DIET , 'Diet'),
+		(WORKOUT , 'Workout')
+	)
+	year = models.IntegerField()
+	week = models.IntegerField()
+	type = models.CharField(
+		max_length = 7,
+		choices = TYPE_CHOICES
+	)
+	created = models.DateTimeField(auto_now_add = True)
+	regenerated = models.DateTimeField(auto_now = True)
