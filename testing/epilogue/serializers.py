4548193f0785e00b676822626d3cf056166413d7
Shikhar Chauhan <shikhar.chauhan@live.com>
2017-12-25 15:02:59 +0530

    Adding diseases and injuries to Customer Serializer

--- testing/epilogue/serializers.py
+++ testing/epilogue/serializers.py
@@ -10,6 +10,9 @@ class ObjectiveSerializer(serializers.ModelSerializer):
 		fields = ["id" , "name"]
 
 class CustomerSerializer(serializers.ModelSerializer):
+
+	diseases = serializers.StringRelatedField(many = True , source = "customermedicalconditions_set")
+	injuries = serializers.StringRelatedField(many = True)
 	height = serializers.CharField(source = "h" , required = False)
 	weight = serializers.CharField(source = "w", required = False)
 	gender = serializers.CharField(source = "gen", required = False)
@@ -19,7 +22,7 @@ class CustomerSerializer(serializers.ModelSerializer):
 
 	class Meta:
 		model = Customer
-		fields = ["email" , "first_name" , "last_name" , "mobile" , "age" , "weight" , "height", "lifestyle" , "objective" , "id", "gender" , "body_type" , "food_cat" ,"weight_type" , "height_type" , "work_pref" , "level"]
+		fields = ["email" , "first_name" , "last_name" , "mobile" , "age" , "weight" , "height", "lifestyle" , "objective" , "id", "gender" , "body_type" , "food_cat" ,"weight_type" , "height_type" , "work_pref" , "level" , "diseases" , "injuries"]
 
 class FoodSerializer(serializers.ModelSerializer):
 	class Meta:
