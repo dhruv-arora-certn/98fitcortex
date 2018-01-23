b6873e1ab3213b0436d5ba85a86c55e00683c7ca
Shikhar Chauhan <shikhar.chauhan@live.com>
2018-01-23 17:59:24 +0530

    Toggling existing entry of regeneration log

--- testing/regeneration/utils.py
+++ testing/regeneration/utils.py
@@ -11,57 +11,60 @@ import logging
 
 
 def get_logger():
-	return logging.getLogger(__name__)
+    return logging.getLogger(__name__)
 
 def get_window_tuples(week = get_week() , year = get_year()):
-	return [
-		(y , w) for w,y in zip(
-			[
-				week + i for i in range(0, 1 + constants.REGENERATION_WINDOW)
-			],
-			itertools.repeat(year , constants.REGENERATION_WINDOW + 1)
-		)
-	]
+    return [
+        (y , w) for w,y in zip(
+            [
+                week + i for i in range(0, 1 + constants.REGENERATION_WINDOW)
+            ],
+            itertools.repeat(year , constants.REGENERATION_WINDOW + 1)
+        )
+    ]
 
 
 def create_regeneration_node(_type, user, year , week):
-	logger = get_logger()
-	logger.debug("++++++++++++++++ Creating Object")
-	obj = models.RegenerationLog.objects.get_or_create(
-		type = _type,
-		year = year,
-		week = week,
-		customer = user
-	)
-	return obj
+    logger = get_logger()
+    logger.debug("++++++++++++++++ Creating Object")
+    obj,created = models.RegenerationLog.objects.get_or_create(
+        type = _type,
+        year = year,
+        week = week,
+        customer = user
+    )
+    if not created:
+        obj.toggleStatus()
+
+    return obj
 
 def create_diet_regeneration_node(user,year,week):
-	logger = get_logger()
-	dietplan = GeneratedDietPlan.objects.filter(
-		year = year,
-		week_id = week,
-		customer = user
-	)
-	count = dietplan.count()
-	logger.debug("Reaching Diet Regeneration Stage count:%d , week:%d , year:%d"%(count,week,year))
-	logger.debug(str(dietplan.query))
-	if count:
-		return create_regeneration_node(
-			"diet" ,user ,year , week
-		)
-	return None
+    logger = get_logger()
+    dietplan = GeneratedDietPlan.objects.filter(
+        year = year,
+        week_id = week,
+        customer = user
+    )
+    count = dietplan.count()
+    logger.debug("Reaching Diet Regeneration Stage count:%d , week:%d , year:%d"%(count,week,year))
+    logger.debug(str(dietplan.query))
+    if count:
+        return create_regeneration_node(
+            "diet" ,user ,year , week
+        )
+    return None
 
 def create_workout_regeneration_node(user,year,week):
-	logger = get_logger()
-	logger.debug("Reaching Workout Regeneration Stage")
+    logger = get_logger()
+    logger.debug("Reaching Workout Regeneration Stage")
 
-	workoutplan = GeneratedExercisePlan.objects.filter(
-		year = year,
-		week_id  = week,
-		customer = user
-	)
-	if workoutplan.count():
-		return create_regeneration_node(
-			"workout" , user ,year , week
-		)
-	return None
+    workoutplan = GeneratedExercisePlan.objects.filter(
+        year = year,
+        week_id  = week,
+        customer = user
+    )
+    if workoutplan.count():
+        return create_regeneration_node(
+            "workout" , user ,year , week
+        )
+    return None
