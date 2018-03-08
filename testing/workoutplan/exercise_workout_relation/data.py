af886a9525ed5f5783177e2b0344be3edfd412a7
Shikhar Chauhan <shikhar.chauhan@live.com>
2018-03-08 13:59:57 +0530

    Progress

--- testing/workoutplan/exercise_workout_relation/data.py
+++ testing/workoutplan/exercise_workout_relation/data.py
@@ -1,7 +1,32 @@
 from workoutplan import levels
 from dietplan import activity
+from collections import namedtuple
 
+
+activtiy_tuple = namedtuple("name" , "value")
+
+Sedentary = activity_tuple(
+    "Sedentary" , 
+    activity.ActivityLevel.sedentary
+)
+
+LightlyActive = activity_tuple(
+    "LightlyActive",
+    activity.ActivityLevel.lightly_active
+)
+
+ModeratelyActive = activity_tuple(
+    "Moderately Active",
+    activity.ActivityLevel.moderately_active
+)
 
 activity_level_map = {
-    ()
+    levels.Novice : [
+        (
+            Sedentary , 0
+        ),
+        (
+            LightlyActive , 1
+        )
+    ]
 }
