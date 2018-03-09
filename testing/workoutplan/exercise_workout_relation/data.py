c6cf580030843c1d22ba6358bfc90efe5bd531d3
Shikhar Chauhan <shikhar.chauhan@live.com>
2018-03-09 17:02:21 +0530

    Check for activity

--- testing/workoutplan/exercise_workout_relation/data.py
+++ testing/workoutplan/exercise_workout_relation/data.py
@@ -1,32 +1,38 @@
 from workoutplan import levels
-from dietplan import activity
+from dietplan.activity import ActivityLevel
 from collections import namedtuple
 
 
-activtiy_tuple = namedtuple("name" , "value")
+activity_tuple = namedtuple("Activity", ["name" , "value"])
 
-Sedentary = activity_tuple(
-    "Sedentary" , 
-    activity.ActivityLevel.sedentary
-)
 
-LightlyActive = activity_tuple(
-    "LightlyActive",
-    activity.ActivityLevel.lightly_active
-)
 
-ModeratelyActive = activity_tuple(
-    "Moderately Active",
-    activity.ActivityLevel.moderately_active
-)
+##### Activity-Level Validation Map
+# 4x3 matrix
+# Rows(Lifestyle/ActivityLevel) : Sedentary, LightlyActive, ModeratelyActive, VeryActive
+# Columns(Fitness Level) : Novice , Beginner, Intermediate
+# activity_level_map[1,1] is retrieval of Compatibility of LightlyActive and Beginner
+#
 
-activity_level_map = {
-    levels.Novice : [
-        (
-            Sedentary , 0
-        ),
-        (
-            LightlyActive , 1
-        )
+activity_level_map = [
+    [
+        (ActivityLevel.lightly_active, 0) , (ActivityLevel.lightly_active, 1) , ()
+    ],
+    [
+        (ActivityLevel.lightly_active, 0) , (ActivityLevel.moderately_active, 0) , (ActivityLevel.moderately_active, 1)
+    ],
+    [
+        (ActivityLevel.moderately_active, 0) , (ActivityLevel.moderately_active, 0) , (ActivityLevel.moderately_active, 1)
+    ],
+    [
+        (ActivityLevel.very_active, 0) , (ActivityLevel.very_active, 0) , (ActivityLevel.very_active, 0)
     ]
-}
+]
+
+
+def check_activity_fitness_compat(activity , fitness):
+    act , periodize = activity_level_map[1][0]
+    changed = False
+    if not act == activity:
+        changed = True
+    return act,periodize,changed
