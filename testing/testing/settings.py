8d89792b7d0566ab7f6203348f2c6ec3e4aae44f
Shikhar Chauhan <shikhar.chauhan@live.com>
2018-04-10 13:03:12 +0530

    Fix the timezone

--- testing/testing/settings.py
+++ testing/testing/settings.py
@@ -141,12 +141,12 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-#TIME_ZONE = os.environ.get("time_zone")
+TIME_ZONE = os.environ.get("time_zone")
 
 USE_I18N = True
 USE_L10N = True
 
-#USE_TZ = True
+USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
