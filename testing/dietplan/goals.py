55cf245c21731fe21dcabaf6b8b52407fb574e92
Shikhar Chauhan <shikhar.chauhan@live.com>
2018-05-14 15:09:18 +0530

    Fixing tabbing

--- testing/dietplan/goals.py
+++ testing/dietplan/goals.py
@@ -1,37 +1,43 @@
 class Base():
-	@classmethod
-	def get_attributes(self):
-		return [self.protein , self.carbs , self.fat , self.field]
+    @classmethod
+    def get_attributes(self):
+        return [self.protein , self.carbs , self.fat , self.field]
 
 class Goals:
-	
-	class WeightLoss(Base):
-		_diff = -0.15
-		protein = 0.20
-		fat = 0.25
-		carbs = 0.55
-		field = "squared_diff_weight_loss"
-		
-		def __str__(self):
-			return "Weight Loss"
+    
+    class WeightLoss(Base):
+        _diff = -0.15
+        protein = 0.20
+        fat = 0.25
+        carbs = 0.55
+        field = "squared_diff_weight_loss"
+        
+        def __str__(self):
+            return "Weight Loss"
 
-	class WeightGain(Base):
-		_diff = 0.10
-		protein = 0.15
-		fat = 0.30
-		carbs = 0.55
-		field = "squared_diff_weight_gain"
+    class WeightGain(Base):
+        _diff = 0.10
+        protein = 0.15
+        fat = 0.30
+        carbs = 0.55
+        field = "squared_diff_weight_gain"
 
-	class MuscleGain(Base):
-		_diff = 0.10
-		protein = 0.25
-		fat = 0.25
-		carbs = 0.50
-		field = "squared_diff_weight_gain"
+    class MuscleGain(Base):
+        _diff = 0.10
+        protein = 0.25
+        fat = 0.25
+        carbs = 0.50
+        field = "squared_diff_weight_gain"
 
-	class MaintainWeight(Base):
-		_diff = 0
-		protein = 0.15
-		fat = 0.25
-		carbs = 0.60
-		field = "squared_diff_weight_maintain"
+    class MaintainWeight(Base):
+        _diff = 0
+        protein = 0.15
+        fat = 0.25
+        carbs = 0.60
+        field = "squared_diff_weight_maintain"
+
+    class NotSet:
+        '''
+        To be used when goal is not set
+        '''
+        pass
