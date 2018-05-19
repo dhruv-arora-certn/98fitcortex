4374547ee1ed839105d1b96d268b514c22db28a2
Shikhar Chauhan <shikhar.chauhan@live.com>
2018-05-19 17:55:21 +0530

    Adding user creation date in user serializer

--- testing/epilogue/serializers.py
+++ testing/epilogue/serializers.py
@@ -51,7 +51,7 @@ class CustomerSerializer( CustomerUpdateMixin , serializers.ModelSerializer):
 
     class Meta:
         model = Customer
-        fields = ["email" , "first_name" , "last_name" , "mobile" , "age" , "weight" , "height", "lifestyle" , "objective" , "id", "gender" , "body_type" , "food_cat" ,"weight_type" , "height_type" , "work_pref" , "level" , "diseases" , "injuries" , "reasons", "new_latest_activity", "current_level", "level_in_use"]
+        fields = ["email" , "first_name" , "last_name" , "mobile" , "age" , "weight" , "height", "lifestyle" , "objective" , "id", "gender" , "body_type" , "food_cat" ,"weight_type" , "height_type" , "work_pref" , "level" , "diseases" , "injuries" , "reasons", "new_latest_activity", "current_level", "level_in_use", "create_on"]
 
     def get_reasons(self, obj):
         return str(obj.reasons.last())
