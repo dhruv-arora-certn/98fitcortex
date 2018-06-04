04a52ffaeb6299acd9d66ac1dcd6f442747b804e
Shikhar Chauhan <shikhar.chauhan@live.com>
2018-06-04 17:22:06 +0530

    Groundwork for dietplan caching

--- testing/epilogue/cache_utils.py
+++ testing/epilogue/cache_utils.py
@@ -11,10 +11,13 @@ modules = types.SimpleNamespace()
 
 modules.SLEEP_LOGS = "sleep_logs"
 modules.SLEEP_AGGREGATE = "sleep_aggregate"
+modules.SLEEP = "sleep"
 modules.ACTIVITY_LOGS = "activity_logs"
 modules.ACTIVITY_AGGREGATE = "activity_aggregate"
+modules.ACTIVITY = "activity"
 modules.DIET_DASHBOARD_STRING = "diet_dashboard_string"
-modules.SLEEP = "sleep"
+modules.DIET_PLAN = "diet_plan"
+modules.DIET = "diet"
 
 @functools.lru_cache()
 def get_cache_key(user, module):
@@ -27,7 +30,9 @@ def get_cache_key(user, module):
         modules.ACTIVITY_LOGS : get_activity_logs_cache_key,
         modules.ACTIVITY_AGGREGATE : get_activity_aggregate_cache_key,
         modules.DIET_DASHBOARD_STRING : get_diet_dashboard_string_cache_key,
-        modules.SLEEP : get_sleep_cache_keys
+        modules.SLEEP : get_sleep_cache_keys,
+        modules.DIET_PLAN : get_dietplan_cache_key,
+        modules.DIET : get_diet_cache_keys
     } 
     return data.get(module)(user)
 
@@ -62,12 +67,29 @@ def get_diet_dashboard_string_cache_key(user):
     )
 
 @functools.lru_cache()
+def get_dietplan_cache_key(user):
+    return KEY_TEMPLATE%(
+        user.id, modules.DIET_PLAN
+    )
+
+@functools.lru_cache()
 def get_sleep_cache_keys(user):
     '''
     Return all the cache keys for a user belonging to a particular module
     '''
     return [
         KEY_TEMPLATE%(user.id, e) for e in [modules.SLEEP_LOGS, modules.SLEEP_AGGREGATE]
+    ]
+
+@functools.lru_cache()
+def get_diet_cache_keys(user):
+    '''
+    Return all the keys for a user belonging to diet module
+    '''
+    return [
+        KEY_TEMPLATE%(user.id, e) for e in [
+            modules.DIET_DASHBOARD_STRING, modules.DIET_PLAN
+        ]
     ]
 
 def get_time_to_midnight(time = None):
